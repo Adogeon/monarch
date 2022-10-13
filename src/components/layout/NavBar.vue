5e04db8dc1d04fb11e7200dee66b05817e6bebc4
Anh <addao823@gmail.com>
2022-10-13 16:48:15 -0700

    Adding About page with safety feature

--- src/components/layout/NavBar.vue
+++ src/components/layout/NavBar.vue
@@ -49,6 +49,10 @@
         <!-- Left links -->
         <ul class="navbar-nav flex flex-col items-end pl-0 list-style-none ml-auto">
           <li class="nav-item pr-2">
+            <router-link class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="/service">Service
+            </router-link>
+          </li>
+          <li class="nav-item pr-2">
             <router-link class="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="/service">Service</router-link>
           </li>
           <li class="nav-item pr-2">
--- src/views/router.js
+++ src/views/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 
 import ContactPage from "./ContactPage.vue";
+import AboutPage from "./AboutPage.vue";
 import HomePage from "./HomePage.vue";
 import ProjectPage from "./ProjectPage.vue";
 import ServicePage from "./ServicePage.vue";
 
 const routes = [
 	{ path: "/", component: HomePage },
+	{ path: "/about", component: AboutPage },
 	{ path: "/project", component: ProjectPage },
 	{ path: "/service", component: ServicePage },
 	{ path: "/contact", component: ContactPage },
