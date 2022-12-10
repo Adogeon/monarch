3ffc68d759d9219286252cfb88cd8c5a1b2e5733
Adogeon <addao823@gmail.com>
2022-12-09 16:45:12 -0800

    adding dist

--- src/components/layout/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-<footer class="bg-gray-200 text-center mt-6 lg:text-left">
-  <div class="text-gray-700 text-center p-4" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2022 Copyright:
-    <router-link class="text-gray-800" to="/">Monarch Industry</router-link>
-  </div>
-</footer>
-</template>
